afaf8c967011b75a921b19906b623047e19961e9
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-08-18 10:18:09 +0200

    fix(example): update asset path extension

--- examples/choices.rs
+++ examples/choices.rs
@@ -35,7 +35,7 @@ fn main() {
 }
 
 fn load_talks(mut commands: Commands, server: Res<AssetServer>) {
-    let h: Handle<RawScreenplay> = server.load("choices.json");
+    let h: Handle<RawScreenplay> = server.load("choices.screenplay.json");
     commands.insert_resource(SimpleScreenplayAsset { handle: h });
 }
 
