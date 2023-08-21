78de26642dc4db626c597b996b0ec384db5a9067
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-08-21 18:36:21 +0200

    fix: finish examples updates

--- examples/full.rs
+++ examples/full.rs
@@ -35,7 +35,7 @@ fn main() {
 }
 
 fn load_talks(mut commands: Commands, server: Res<AssetServer>) {
-    let h: Handle<RawScreenplay> = server.load("full.screenplay.json");
+    let h: Handle<RawScreenplay> = server.load("full.screenplay.ron");
     commands.insert_resource(SimpleScreenplayAsset { handle: h });
 }
 
