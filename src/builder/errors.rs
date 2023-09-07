7042e2806e6caee2d9699c8a16f50241ca375ee0
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-09-07 09:45:43 +0200

    refactor: remove old json error enum

--- src/builder/errors.rs
+++ src/builder/errors.rs
@@ -1,16 +1,6 @@
 //! Errors for the builder module
 
 use thiserror::Error;
-/// Errors when parsing a Talk json
-#[derive(Error, Debug, PartialEq, Eq)]
-pub enum JsonError {
-    /// Serde failed to parse the json
-    #[error("serde failed to parse the json: {0}")]
-    BadParse(String),
-    /// The Talk json is not valid
-    #[error("the script is not valid: {0:?}")]
-    Validation(Vec<String>),
-}
 
 /// Errors when building a Talk
 #[derive(Error, Debug, PartialEq, Eq)]
