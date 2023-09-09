93c80556a141a5b51a6b7a79afbdd3fd337cb416
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-09-09 11:29:59 +0200

    update ingame example

--- examples/ingame.rs
+++ examples/ingame.rs
@@ -1,6 +1,9 @@
 use bevy::{asset::LoadState, prelude::*};
 use bevy_talks::prelude::*;
 
+#[derive(Component, Default)]
+struct ActiveTalk(bool);
+
 #[derive(States, Default, Debug, Clone, Eq, PartialEq, Hash)]
 enum AppState {
     #[default]
@@ -10,8 +13,8 @@ enum AppState {
 
 #[derive(Resource)]
 struct TalkAsset {
-    sp_a: Handle<RawTalk>,
-    sp_b: Handle<RawTalk>,
+    bev_talk_handle: Handle<RawTalk>,
+    feri_talk_handle: Handle<RawTalk>,
 }
 
 #[derive(Component)]
@@ -19,6 +22,9 @@ struct Player;
 
 #[derive(Component)]
 struct Interactable;
+
+#[derive(Component)]
+struct Dialogue;
 
 fn main() {
     App::new()
@@ -32,7 +38,14 @@ fn main() {
         .add_systems(OnEnter(AppState::Loaded), setup)
         .add_systems(
             Update,
-            (move_player, interact, print).run_if(in_state(AppState::Loaded)),
+            (
+                move_player,
+                interact,
+                print,
+                deactive_talk_when_far,
+                advance_convo,
+            )
+                .run_if(in_state(AppState::Loaded)),
         )
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
@@ -41,7 +54,10 @@ fn main() {
 fn load_talks(mut commands: Commands, server: Res<AssetServer>) {
     let sp_a: Handle<RawTalk> = server.load("talks/interact_a.talk.ron");
     let sp_b: Handle<RawTalk> = server.load("talks/interact_b.talk.ron");
-    commands.insert_resource(TalkAsset { sp_a, sp_b });
+    commands.insert_resource(TalkAsset {
+        bev_talk_handle: sp_a,
+        feri_talk_handle: sp_b,
+    });
 }
 
 fn check_loading(
@@ -49,31 +65,18 @@ fn check_loading(
     sp_asset: Res<TalkAsset>,
     mut next_state: ResMut<NextState<AppState>>,
 ) {
-    let load_state_a = server.get_load_state(&sp_asset.sp_a);
-    let load_state_b = server.get_load_state(&sp_asset.sp_b);
+    let load_state_a = server.get_load_state(&sp_asset.bev_talk_handle);
+    let load_state_b = server.get_load_state(&sp_asset.feri_talk_handle);
     if load_state_a == LoadState::Loaded && load_state_b == LoadState::Loaded {
         next_state.set(AppState::Loaded);
     }
 }
 
-// fn setup_Talk(
-//     mut commands: Commands,
-//     raws: Res<Assets<RawTalk>>,
-//     sp_asset: Res<TalkAsset>,
-// ) {
-//     let raw_sp = raws.get(&sp_asset.handle).unwrap();
-//     let Talk = TalkBuilder::new().build(&raw_sp).unwrap();
-
-//     commands.spawn(Talk);
-//     println!();
-//     println!("Press space to advance the conversation. And 1, 2 to pick a choice.");
-// }
-
 fn setup(
     mut commands: Commands,
     assets: Res<AssetServer>,
     raws: Res<Assets<RawTalk>>,
-    sp_asset: Res<TalkAsset>,
+    talk_asset: Res<TalkAsset>,
 ) {
     commands.spawn(Camera2dBundle::default());
     let player_handle: Handle<Image> = assets.load("images/player.png");
@@ -87,35 +90,63 @@ fn setup(
         },
     ));
 
-    let a: Handle<Image> = assets.load("images/A.png");
-    let b: Handle<Image> = assets.load("images/B.png");
-    let raw_sp_a = raws.get(&sp_asset.sp_a).unwrap();
-    let raw_sp_b = raws.get(&sp_asset.sp_b).unwrap();
+    let bev: Handle<Image> = assets.load("images/bev.png");
+    let raw_talk_bev = raws.get(&talk_asset.bev_talk_handle).unwrap();
     commands.spawn((
+        ActiveTalk(false),
         Interactable,
         SpriteBundle {
-            texture: a,
-            transform: Transform::from_scale(Vec3::splat(2.))
+            texture: bev,
+            transform: Transform::from_scale(Vec3::splat(3.))
                 .with_translation(Vec3::new(-300., 0., 1.)),
             ..default()
         },
         TalkerBundle {
-            talk: Talk::build(&raw_sp_a).unwrap(),
+            talk: Talk::build(&raw_talk_bev).unwrap(),
             ..default()
         },
     ));
+
+    let feri: Handle<Image> = assets.load("images/feri.png");
+    let raw_talk_feri = raws.get(&talk_asset.feri_talk_handle).unwrap();
     commands.spawn((
+        ActiveTalk(false),
         Interactable,
         SpriteBundle {
-            texture: b,
-            transform: Transform::from_scale(Vec3::splat(2.))
+            texture: feri,
+            transform: Transform::from_scale(Vec3::splat(3.))
                 .with_translation(Vec3::new(300., 0., 1.)),
             ..default()
         },
         TalkerBundle {
-            talk: Talk::build(&raw_sp_b).unwrap(),
+            talk: Talk::build(&raw_talk_feri).unwrap(),
             ..default()
         },
+    ));
+
+    // the ui for the talks
+    commands.spawn((
+        Dialogue,
+        TextBundle::from_section(
+            "A-D to move. E near a character to interact. Space to advance convo.",
+            TextStyle {
+                font_size: 30.0,
+                color: Color::WHITE,
+                ..default()
+            },
+        )
+        .with_text_alignment(TextAlignment::Center)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.),
+            margin: UiRect {
+                left: Val::Auto,
+                right: Val::Auto,
+                bottom: Val::Px(20.),
+                ..default()
+            },
+            ..default()
+        }),
     ));
 }
 
@@ -134,56 +165,83 @@ fn move_player(
     }
 }
 
+fn advance_convo(
+    input: Res<Input<KeyCode>>,
+    mut next_action_events: EventWriter<NextActionRequest>,
+    talks: Query<(Entity, &ActiveTalk), With<Talk>>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        for (entity, active) in talks.iter() {
+            if active.0 {
+                next_action_events.send(NextActionRequest(entity));
+            }
+        }
+    }
+}
+
 fn interact(
     input: Res<Input<KeyCode>>,
     player_query: Query<&Transform, With<Player>>,
-    mut interactable_query: Query<(&Transform, &mut Activated), With<Interactable>>,
+    mut talks: Query<(Entity, &Transform, &mut ActiveTalk), With<Interactable>>,
+    mut init_talk_events: EventWriter<InitTalkRequest>,
 ) {
     if input.just_pressed(KeyCode::E) {
         let player_transform = player_query.single();
-
-        for (transform, mut active) in interactable_query.iter_mut() {
+        for (entity, transform, mut active) in talks.iter_mut() {
             if transform.translation.distance(player_transform.translation) < 100. {
-                active.0 = !active.0;
-                info!("Interacted with an interactable!");
+                if active.0 {
+                    active.0 = false;
+                } else {
+                    active.0 = true;
+                    init_talk_events.send(InitTalkRequest(entity));
+                }
+            }
+        }
+    }
+}
+
+fn deactive_talk_when_far(
+    player_query: Query<&Transform, With<Player>>,
+    mut talks: Query<(&Transform, &mut ActiveTalk)>,
+) {
+    let player_transform = player_query.single();
+    for (transform, mut active) in talks.iter_mut() {
+        if transform.translation.distance(player_transform.translation) > 100. {
+            if active.0 {
+                active.0 = false;
             }
         }
     }
 }
 
 fn print(
-    talk_comps: Query<(
-        Ref<CurrentText>,
-        &CurrentActors,
-        &CurrentNodeKind,
-        &CurrentChoices,
-    )>,
+    talk_comps: Query<(Ref<CurrentText>, &CurrentActors, Ref<ActiveTalk>)>,
+    mut texts: Query<&mut Text, With<Dialogue>>,
 ) {
-    for (tt, ca, kind, cc) in talk_comps.iter() {
-        if !tt.is_changed() || tt.is_added() {
+    for (tt, ca, active) in talk_comps.iter() {
+        // skip if comps were just added
+        if active.is_added() || tt.is_added() {
             continue;
         }
-        // extract actors names into a vector
-        let actors =
-            ca.0.iter()
-                .map(|a| a.name.to_owned())
-                .collect::<Vec<String>>();
 
-        let mut speaker = "Narrator";
-        if actors.len() > 0 {
-            speaker = actors[0].as_str();
+        // If talk was deactivated, clear the text
+        if active.is_changed() && !active.0 {
+            texts.single_mut().sections[0].value =
+                "A-D to move. E near a character to interact. Space to advance convo.".to_string();
+            continue;
         }
 
-        match kind.0 {
-            TalkNodeKind::Talk => println!("{}: {}", speaker, tt.0),
-            TalkNodeKind::Join => println!("--- {actors:?} enters the scene."),
-            TalkNodeKind::Leave => println!("--- {actors:?} exit the scene."),
-            TalkNodeKind::Choice => {
-                println!("Choices:");
-                for (i, choice) in cc.0.iter().enumerate() {
-                    println!("{}: {}", i + 1, choice.text);
-                }
-            }
+        // skip if text was not changed
+        if !tt.is_changed() {
+            continue;
+        }
+
+        let speaker = if ca.0.len() > 0 {
+            ca.0[0].name.as_str()
+        } else {
+            "Narrator"
         };
+
+        texts.single_mut().sections[0].value = format!("{}: {}", speaker, tt.0)
     }
 }
