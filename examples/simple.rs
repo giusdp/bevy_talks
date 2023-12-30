72424cb74878bc6ec23df04a032a673e16d947c1
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-12-30 18:18:11 +0100

    refactor(examples): update simple example with new version features

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,7 +10,7 @@ enum AppState {
 
 #[derive(Resource)]
 struct SimpleTalkAsset {
-    handle: Handle<RawTalk>,
+    handle: Handle<TalkData>,
 }
 
 fn main() {
@@ -24,11 +24,12 @@ fn main() {
             Update,
             (interact, print, bevy::window::close_on_esc).run_if(in_state(AppState::Loaded)),
         )
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
 fn load_talks(mut commands: Commands, server: Res<AssetServer>) {
-    let h: Handle<RawTalk> = server.load("talks/simple.talk.ron");
+    let h: Handle<TalkData> = server.load("talks/simple.talk.ron");
     commands.insert_resource(SimpleTalkAsset { handle: h });
 }
 
@@ -45,18 +46,14 @@ fn check_loading(
 
 fn setup_talk(
     mut commands: Commands,
-    raws: Res<Assets<RawTalk>>,
-    simple_sp_asset: Res<SimpleTalkAsset>,
-    mut init_talk_events: EventWriter<InitTalkRequest>,
+    talks: Res<Assets<TalkData>>,
+    simple_talk_asset: Res<SimpleTalkAsset>,
 ) {
-    let raw_sp = raws.get(&simple_sp_asset.handle).unwrap();
-    let talk = Talk::build(&raw_sp).unwrap();
+    let simple_talk = talks.get(&simple_talk_asset.handle).unwrap();
+    let talk_builder = TalkBuilder::default().into_builder(simple_talk);
+    commands.add(talk_builder.build());
 
-    let e = commands.spawn(TalkerBundle { talk, ..default() }).id();
-
-    init_talk_events.send(InitTalkRequest(e));
-
-    println!();
+    println!("-----------------------------------------");
     println!("Press space to advance the conversation.");
     println!("-----------------------------------------");
 }
@@ -67,44 +64,28 @@ fn interact(
     talks: Query<Entity, With<Talk>>,
 ) {
     if input.just_pressed(KeyCode::Space) {
-        let e = talks.single();
-        next_action_events.send(NextActionRequest(e));
+        next_action_events.send(NextActionRequest(talks.single()));
     }
 }
 
-fn print(
-    talk_comps: Query<(
-        Ref<CurrentText>,
-        &CurrentActors,
-        &CurrentNodeKind,
-        &CurrentChoices,
-    )>,
-) {
-    for (tt, ca, kind, cc) in talk_comps.iter() {
-        if !tt.is_changed() || tt.is_added() {
+fn print(talk_comps: Query<Ref<Talk>>) {
+    for talk in &talk_comps {
+        if !talk.is_changed() || talk.is_added() {
             continue;
         }
-        // extract actors names into a vector
-        let actors =
-            ca.0.iter()
-                .map(|a| a.name.to_owned())
-                .collect::<Vec<String>>();
+
+        let actors = &talk.current_actors;
 
         let mut speaker = "Narrator";
-        if actors.len() > 0 {
-            speaker = actors[0].as_str();
+        if !talk.current_actors.is_empty() {
+            speaker = &talk.current_actors[0];
         }
 
-        match kind.0 {
-            TalkNodeKind::Talk => println!("{}: {}", speaker, tt.0),
-            TalkNodeKind::Join => println!("--- {actors:?} enters the scene."),
-            TalkNodeKind::Leave => println!("--- {actors:?} exit the scene."),
-            TalkNodeKind::Choice => {
-                println!("Choices:");
-                for (i, choice) in cc.0.iter().enumerate() {
-                    println!("{}: {}", i + 1, choice.text);
-                }
-            }
+        match talk.current_kind {
+            NodeKind::Talk => println!("{speaker}: {}", talk.current_text),
+            NodeKind::Join => println!("--- {actors:?} enters the scene."),
+            NodeKind::Leave => println!("--- {actors:?} exit the scene."),
+            NodeKind::Choice => println!("Not implemented"),
         };
     }
 }
